0ba28a25254a6d3c994792c3c1718f17e5f94f13
RUHUL AMIN <ruhulaminn1316@gmail.com>
2025-03-14 21:18:01 +0600

    even chack

---
+++ pb-08.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter your number: ");
+    scanf("%d",&a);
+    if (a % 2 == 0)
+    {
+        /* codprinte */printf("even\n");          
+    }
+    
+
+    return 0;
+}
